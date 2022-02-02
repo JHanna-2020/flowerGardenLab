76ea9f553773c3c5e2efee76598f58926c42a315
JHanna-2020 <66224091+JHanna-2020@users.noreply.github.com>
2022-02-02 13:24:44 -0600

    Add files via upload

---
+++ FlowerGarden.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class FlowerGarden
+{
+    private ArrayList  <String> s= new ArrayList<String>();
+    //create an arraylist instance variable
+    
+    //write a constructor         
+    public FlowerGarden(ArrayList<String> list){
+        s = list; 
+    }
+    //write add flower
+    public String addFlower(String userInput){
+        
+        
+        
+    }
+    //write get count
+        
+    //write get flower name
+    
+    //write in range
+    
+    //write find flower
+
+    //write max flower    
+    
+    //write a toString
+}
